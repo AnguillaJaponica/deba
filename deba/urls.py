8aab3c06aef52b8305e308cd14fa172cd9eadb98
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2022-06-05 23:50:30 +0900

    install fish_dictionaries#index page

--- deba/urls.py
+++ deba/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path # include: 他のURLconfへの参照を可能にする
 
 urlpatterns = [
+    path('fish_dictionaries/', include('fish_dictionaries.urls')),
     path('admin/', admin.site.urls),
 ]
